7c3a6f40afaf39fef0f301a62f436c0ac03d6abb
Raul <jimenezraul1981@gmail.com>
2022-12-01 23:06:04 -0500

    first commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ManyToAny;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static jakarta.persistence.FetchType.*;
+import static jakarta.persistence.GenerationType.AUTO;
+
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id @GeneratedValue(strategy = AUTO)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @ManyToAny(fetch = EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+}
